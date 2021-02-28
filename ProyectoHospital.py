a86aa3ceafb387a665e917de381bb0734ebb786c
JCapurro <juan.capurro12@gmail.com>
2021-02-28 02:18:53 -0300

    Ahora la base de pacientes se guarda en un archivo para poder seguir utilizandola una vez cerrado el programa. Ademas, ahora se puede agregar historial a un paciente pre existente

--- ProyectoHospital.py
+++ ProyectoHospital.py
@@ -1,11 +1,15 @@
-import os
+import os,csv
 os.system("cls")
 print("\n\n\t\t----BIENVENIDO AL PROGRAMA----\n\n")
 running=True
-database=[{'Nombre': 'Juan', 'Edad': 18, 'Historial': 'Malestar estomacal'}, {'Nombre': 'Pepe', 'Edad': 54, 'Historial': 'Dolor de cabeza'}, {'Nombre': 'Pablo', 'Edad': 44, 'Historial': 'Ronchas en la piel'}]
+database=[]
+with open('DB.csv') as data:
+    reader = csv.reader(data)
+    for line in reader:
+        database.append(line)
 
 def show_menu():
-    print("1-Cargar paciente\n2-Buscar paciente por nombre\n3-Listar pacientes\n4-Salir del programa")
+    print("1-Cargar paciente\n2-Buscar paciente por nombre\n3-Listar pacientes\n4-Actualizar historial de paciente\n5-Salir del programa")
     action=input("Elija la opcion que desee: ")
     os.system("cls")
     return action
@@ -16,7 +20,7 @@ def validate_response(n):
 
     if n.isdigit():
         num_res=int(n)
-        if num_res >0 and num_res < 5:
+        if num_res >0 and num_res < 6:
             msg="en rango"
             validate=True
         else:
@@ -25,48 +29,69 @@ def validate_response(n):
         msg="no es numero"
     return validate,num_res,msg
 
-def carga_paciente():
+def carga_paciente(database):
     name=input("Ingrese el nombre: ")
     age=int(input("Ingrese la edad: "))
     history=input("Ingrese la historia clinica: ")
-    paciente={'Nombre': name,'Edad':age,'Historial':history}
+    paciente =[name,age,history]
     return paciente
+    
+
 
 def buscar_paciente(database,name):
     encontrado=False
     nombre=""
-    info={}
+    info=[]
+    index=0
     for i in range(len(database)):
-            if database[i]["Nombre"].lower()==name.lower():
-                nombre = name
-                info=database[i]
-                encontrado=True
-    return nombre,info,encontrado
-
-#def listar_paciente(database):
-
+        if database[i][0].lower()==name.lower():
+            nombre = name
+            info=database[i]
+            index=i
+            encontrado=True
+    return nombre,info,encontrado,index
 
 while running:
     response = show_menu()
     validate,num_res,msg=validate_response(response)
     if validate:
         if num_res==1:
-            paciente=carga_paciente()
+            paciente=carga_paciente(database)
             database.append(paciente)
+            print(database)
             print("\t\t----Paciente agregado----")
         elif num_res==2:
             os.system("cls")
             name=input("Ingrese el nombre del paciente que desea buscar: ")
-            nombre,info,encontrado=buscar_paciente(database,name)
+            nombre,info,encontrado,index=buscar_paciente(database,name)
             if encontrado:
-                print("Aqui tiene la informacion de ",nombre,"\n","Edad:", info["Edad"],"\n","Historial:",info["Historial"],"\n")
+                print("Aqui tiene la informacion de ",nombre,"\n","Edad:", info[1],"\n","Historial:",info[2],"\n")
             else:
                 print("\n\n----Paciente no encontrado----\n\n")
         elif num_res==3:
             os.system("cls")
             for i in range(len(database)):
-                print("\n Nombre: ", database[i]["Nombre"],"\n","Edad: ", database[i]["Edad"],"\n","Historial: ",database[i]["Historial"],"\n\n--------------------------\n")
+                print("\n Nombre: ", database[i][0],"\n","Edad: ", database[i][1],"\n","Historial: ",database[i][2],"\n\n--------------------------\n")
+        elif num_res==4:
+            os.system('cls')
+            name=input("Ingrese el nombre del paciente que desea buscar: ")
+            nombre,info,encontrado,index=buscar_paciente(database,name)
+            if encontrado:
+                new_historial=input("Historial nuevo: ")
+                historial=str(info[2])+"|"+new_historial
+                print(historial)
+                paciente=[nombre,info[1],historial]
+                database.insert(index,paciente)
+                database.pop(index+1)
+                
+            else:
+                print("\n\n----Paciente no encontrado----\n\n")
+
         else:
+            with open("DB.csv",'w',newline='') as db:
+                write=csv.writer(db)
+                for i in range(len(database)):
+                    write.writerow(database[i])
             running=False
     else:
         print("\n\n---Opcion incorrecta---\n\n")
